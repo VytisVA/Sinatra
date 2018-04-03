f5c7928ae84ca276db9458fd27344cbd6dd648da
Vytis Vadoklis <vytis.vadoklis@outlook.com>
2018-04-03 16:47:06 +0100

    borders

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Sinatra
+
+shotgun myapp.rb -p 4567
+
+https://developer.mozilla.org/en-US/docs/Web/CSS/border
+
--- myapp.rb
+++ myapp.rb
@@ -6,5 +6,23 @@ get '/' do
 end
 
 get '/secret' do
-	'testing'
-end	
+	'testing afafaa, asas'
+end
+
+get '/cat' do
+"<div style='  border: 0.5rem outset pink;
+  outline: 0.5rem solid khaki;
+  box-shadow: 0 0 0 2rem skyblue;
+  border-radius: 12px;
+  font: bold 1rem sans-serif;
+  margin: 2rem;
+  padding: 1rem;
+  outline-offset: 0.5rem;'>
+	<img src=http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png>
+	</div>"
+end
+
+# get '/dog' do
+# "<div>I have a border, an outline, AND a box shadow! Amazing, isn't it?</div>"
+
+# end	
