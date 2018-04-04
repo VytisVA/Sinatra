0736835ec2dd2dbc570f1945f524fa41877e7ad1
Vytis Vadoklis <vytis.vadoklis@outlook.com>
2018-04-04 09:34:24 +0100

    cat two

--- myapp.rb
+++ myapp.rb
@@ -10,6 +10,12 @@ get '/secret' do
 end
 
 get '/cat' do
+  "<div style='border: 3px dashed red'>
+     <img src='http://bit.ly/1eze8aE'>
+   </div>"
+end
+
+get '/cat2' do
 "<div style='  border: 0.5rem outset pink;
   outline: 0.5rem solid khaki;
   box-shadow: 0 0 0 2rem skyblue;
@@ -17,12 +23,8 @@ get '/cat' do
   font: bold 1rem sans-serif;
   margin: 2rem;
   padding: 1rem;
-  outline-offset: 0.5rem;'>
-	<img src=http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png>
+  outline-offset: 0.5rem;
+  border-right-width: 50em;'>
+	<img src=http://bit.ly/1eze8aE>
 	</div>"
-end
-
-# get '/dog' do
-# "<div>I have a border, an outline, AND a box shadow! Amazing, isn't it?</div>"
-
-# end	
+end	
